95108da92def656c0fd851bce1e061798d4cee7c
AsHuOvSo <asta@avra.dk>
2020-09-11 10:41:37 +0200

    Initial commit

---
+++ src/GroupNames.java
@@ -0,0 +1,9 @@
+public class GroupNames {
+    public static void main(String[] args) {
+        UdskrivNavn("Asta");
+    }
+
+    public static void UdskrivNavn(String navn) {
+        System.out.println("Gruppemedlem: " + navn);
+    }
+}
